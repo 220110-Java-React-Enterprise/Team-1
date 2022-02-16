87a8adc94fba5ac29d910a92141a48a81ebcbed5
Cheryl See <cheryl.see@revature.net>
2022-02-16 16:48:47 -0600

    Index and login working properly

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,37 @@
 
 
 // Setting up event listener for the index page
-//const arrowElements = document.getElementsByClassName('arrow');
-
 // registration/login prompt on page load
 window.onload = (event) => {
 	console.log("Page is loaded.. register?");
 	// submit button handler
-	const submitButtonNode = document.getElementById('submit');
-	submitButtonNode.addEventListener('click', (event) => {
-		// Take out preventDefault if we want the form to be submitted (this is debug)
+	document.getElementById('submit').addEventListener('click', (event) => {
+		// TODO: Take out preventDefault if we want the form to be submitted (this is debug) *********
 		event.preventDefault();
-		console.log('it was clicked');
+		alert('Submit was clicked');
 	})
+	// Add event handler to registration/login buttons to dismiss login screen
+	const loginButtons = document.getElementsByClassName("login-button");
+	for(let i=0; i<loginButtons.length; i++) {
+		loginButtons[i].addEventListener("click", hideLoginOverlay);
+	}
+	// delay showing login for 3 seconds
+	// Should probably add a check if the person has previously logged in here
+	setTimeout(showLoginOverlay, 1500);
 }
 
+function showLoginOverlay() {
+	console.log("Login overlay displayed");
+	const loginNode = document.getElementById('login');
+	loginNode.classList.remove("hidden");
+	loginNode.classList.add("login-boxes-visible")
+}
 
-/* for (let i=0; i<arrowElements.length; i++) {
-	arrowElements[i].addEventListener('click', showEstimates);
-	// disabled for now since it is spamming the console
-	//arrowElements[i].addEventListener('mouseover', showEstimates);
-} */
+function hideLoginOverlay()
+ {
+	 //TODO: don't forget to remove this later - this is for testing *****************************
+	event.preventDefault();
+	console.log("Login overlay hidden");
+	document.getElementById('login').classList.remove("login-boxes-visible");
+	document.getElementById('login').classList.add("hidden");
+}
